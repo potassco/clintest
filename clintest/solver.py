96e0d8f39dc0ed5c44ede16663bbddbfa967a2c4
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2022-05-24 10:21:17 +0200

    Improve the fit between the Solver and the clingo API

--- clintest/solver.py
+++ clintest/solver.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
+from .evaluator import Evaluator
 from .model import Model
 
 class Solver(ABC):
     @abstractmethod
-    def model(self) -> Optional[Model]:
+    def solve(self, evaluator: Evaluator) -> None:
         pass
