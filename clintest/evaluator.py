8ff1b5a5e65fddf9b99ae49ad0284f3f8ef77dd8
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2022-05-25 00:47:01 +0200

    Add a combinator

--- clintest/evaluator.py
+++ clintest/evaluator.py
@@ -35,3 +35,35 @@ class Evaluator(ABC):
     @abstractmethod
     def conclude(self) -> Result:
         pass
+
+class Combinator(Evaluator):
+    def __init__(self, evaluators: List[Evaluator]):
+        self.ongoing = evaluators
+        self.concluded = dict()
+
+    def on_model(self, model: Model) -> bool:
+        still_ongoing = list()
+        for evaluator in self.ongoing:
+            if evaluator.on_model():
+                still_ongoing.append(evaluator)
+            else:
+                self.concluded.append(evaluator, evaluator.conclude())
+        self.ongoing = still_ongoing
+
+    def on_unsat(self, bounds: Sequence[int]) -> None:
+        for evaluator in self.ongoing:
+            evaluator.on_unsat(bounds)
+
+    def on_statistics(self, statistics: StatisticsMap) -> None:
+        for evaluator in self.ongoing:
+            evaluator.on_statistics(statistics)
+
+    def on_finish(self, result: SolveResult) -> None:
+        for evaluator in self.ongoing:
+            evaluator.on_finish(result)
+
+    def on_core(self, core: Sequence[int]) -> None:
+        for evaluator in self.ongoing:
+            evaluator.on_core(core)
+
+    def conclude(self) -> Result:
