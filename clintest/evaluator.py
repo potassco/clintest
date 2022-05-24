0343b5f825406371be4d6c8bdd8b2e61e399e5c9
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2022-05-24 10:12:58 +0200

    Improve the fit between the Evaluator and the clingo API

--- clintest/evaluator.py
+++ clintest/evaluator.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
+from clingo.solving import SolveResult
+from clingo.statistics import StatisticsMap
 from enum import Enum
-from typing import Optional
+from typing import Optional, Sequence
 
 from .model import Model
 
@@ -9,10 +11,21 @@ class Result(Enum):
     FAILURE = 1
 
 class Evaluator(ABC):
-    @abstractmethod
-    def on_model(self, model: Model) -> Optional[Result]:
+    def on_model(self, model: Model) -> bool:
+        pass
+
+    def on_unsat(self, bounds: Sequence[int]) -> None:
+        pass
+
+    def on_statistics(self, statistics: StatisticsMap) -> None:
+        pass
+
+    def on_finish(self, result: SolveResult) -> None:
+        pass
+
+    def on_core(self, core: Sequence[int]) -> None:
         pass
 
     @abstractmethod
-    def on_finish(self) -> Result:
+    def conclude(self) -> Result:
         pass
