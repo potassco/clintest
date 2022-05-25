90aebbce5184bdfb0623ddb6994b231bdf49881a
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2022-05-25 02:50:53 +0200

    Add an abstract implementation of a solver and concrete on for clingo

---
+++ clintest/solver.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+from clingo.control import Control
+from typing import Sequence
+
+from .assessment import Assessment
+
+class Solver(ABC):
+    @abstractmethod
+    def solve(self, assessment: Assessment) -> None:
+        pass
+
+class Clingo(Solver):
+    def __init__(
+        self,
+        arguments: Sequence[str] = [],
+        files: Sequence[str] = [],
+        program: str = ""
+    ):
+        self.__arguments = arguments
+        self.__files = files
+        self.__program = program
+
+    def __str__(self) -> str:
+        pass # TODO
+
+    def solve(self, assessment: Assessment) -> None:
+        ctl = Control(self.__arguments)
+
+        for file in self.__files:
+            ctl.load(file)
+
+        ctl.add("base", [], self.__program)
+
+        ctl.ground([("base", [])])
+
+        ctl.solve(
+            on_model=assessment.assess_model,
+            on_finish=assessment.assess_result,
+            on_statistics=assessment.assess_statistics
+        )
