7fb165d7dc2d9ebe57faafc7479b04a500db39be
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2022-05-30 14:19:05 +0200

    Add a simple usage example into __main__.py

--- clintest/__main__.py
+++ clintest/__main__.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
-    print("Hello, world")
+    # TODO
+
+    from .solver import Clingo
+    from .assessment import Sat
+
+    assessment = Sat()
+
+    solver = Clingo(arguments=["0"], program="a.")
+    solver.solve(assessment)
+
+    print(assessment)
