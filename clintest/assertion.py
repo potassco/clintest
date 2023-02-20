d04575508b3ce369dd4964da20bc7ba7a86c1977
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-02-20 12:25:26 +0100

    Add various basic assertions
    This includes
      - True_
      - False_
      - Contains
      - Equals
      - SubsetOf
      - SupersetOf

--- clintest/assertion.py
+++ clintest/assertion.py
@@ -1,8 +1,59 @@
 from abc import ABC, abstractmethod
+from typing import Set, Union
+
 from clingo.solving import Model
+from clingo.symbol import Symbol, parse_term
 
 
 class Assertion(ABC):
     @abstractmethod
     def holds_for(self, model: Model) -> bool:
         pass
+
+
+class True_(Assertion):
+    def holds_for(self, model: Model) -> bool:
+        return True
+
+
+class False_(Assertion):
+    def holds_for(self, model: Model) -> bool:
+        return False
+
+
+def _into_symbol(symbol: Union[Symbol, str]) -> Symbol:
+    if isinstance(symbol, Symbol):
+        return symbol
+    return parse_term(symbol)
+
+
+class Contains(Assertion):
+    def __init__(self, symbol: Union[Symbol, str]) -> None:
+        self.__symbol = _into_symbol(symbol)
+
+    def holds_for(self, model: Model) -> bool:
+        return model.contains(self.__symbol)
+
+
+class Equals(Assertion):
+    def __init__(self, symbols: Set[Union[Symbol, str]]) -> None:
+        self.__symbols = {_into_symbol(s) for s in symbols}
+
+    def holds_for(self, model: Model) -> bool:
+        return self.__symbols == set(model.symbols(shown=True))
+
+
+class SubsetOf(Assertion):
+    def __init__(self, symbols: Set[Union[Symbol, str]]) -> None:
+        self.__symbols = {_into_symbol(s) for s in symbols}
+
+    def holds_for(self, model: Model) -> bool:
+        return set(model.symbols(shown=True)).issubset(self.__symbols)
+
+
+class SupersetOf(Assertion):
+    def __init__(self, symbols: Set[Union[Symbol, str]]) -> None:
+        self.__symbols = {_into_symbol(s) for s in symbols}
+
+    def holds_for(self, model: Model) -> bool:
+        return set(model.symbols(shown=True)).issuperset(self.__symbols)
