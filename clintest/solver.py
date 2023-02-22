b8f7b361e9802e73d10894f9492f1dbc905671f4
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-02-22 15:32:36 +0100

    Make it possible to write Clingo("0", "{a; b}.")

--- clintest/solver.py
+++ clintest/solver.py
@@ -16,20 +16,20 @@ class Clingo(Solver):
     def __init__(
         self,
         arguments: Optional[Sequence[str]] = None,
-        files: Optional[Sequence[str]] = None,
         program: Optional[str] = None,
+        files: Optional[Sequence[str]] = None,
     ) -> None:
         self.__arguments = [] if arguments is None else arguments
-        self.__files = [] if files is None else files
         self.__program = "" if program is None else program
+        self.__files = [] if files is None else files
 
     def solve(self, test: Test) -> None:
         ctl = Control(self.__arguments)
 
+        ctl.add("base", [], self.__program)
+
         for file in self.__files:
             ctl.load(file)
-
-        ctl.add("base", [], self.__program)
 
         ctl.ground([("base", [])])
 
