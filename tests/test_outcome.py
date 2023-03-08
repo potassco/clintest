fc6ef6859cc9811321d918f4228565b08482d173
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-03-08 14:15:58 +0100

    Bugfix

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -1,34 +1,37 @@
 from clintest.outcome import Outcome
 
-def test_certain_true():
-    outcome = Outcome(True, True)
-    assert     outcome.current_value()
-    assert     outcome.is_certain()
-    assert     outcome.as_tuple() == (True, True)
-    assert     outcome.is_certainly_true()
-    assert not outcome.is_certainly_false()
 
-def test_uncertain_true():
-    outcome = Outcome(True, False)
-    assert     outcome.current_value()
-    assert not outcome.is_certain()
-    assert     outcome.as_tuple() == (True, False)
-    assert not outcome.is_certainly_true()
-    assert not outcome.is_certainly_false()
-
-def test_uncertain_false():
+def test_false_immutable():
     outcome = Outcome(False, False)
     assert not outcome.current_value()
-    assert not outcome.is_certain()
+    assert not outcome.is_mutable()
     assert     outcome.as_tuple() == (False, False)
-    assert not outcome.is_certainly_true()
-    assert not outcome.is_certainly_false()
+    assert not outcome.is_immutably_true()
+    assert     outcome.is_immutably_false()
 
 
-def test_certain_false():
+def test_false_mutable():
     outcome = Outcome(False, True)
     assert not outcome.current_value()
-    assert     outcome.is_certain()
+    assert     outcome.is_mutable()
     assert     outcome.as_tuple() == (False, True)
-    assert not outcome.is_certainly_true()
-    assert     outcome.is_certainly_false()
+    assert not outcome.is_immutably_true()
+    assert not outcome.is_immutably_false()
+
+
+def test_true_immutable():
+    outcome = Outcome(True, False)
+    assert     outcome.current_value()
+    assert not outcome.is_mutable()
+    assert     outcome.as_tuple() == (True, False)
+    assert     outcome.is_immutably_true()
+    assert not outcome.is_immutably_false()
+
+
+def test_true_mutable():
+    outcome = Outcome(True, True)
+    assert     outcome.current_value()
+    assert     outcome.is_mutable()
+    assert     outcome.as_tuple() == (True, True)
+    assert not outcome.is_immutably_true()
+    assert not outcome.is_immutably_false()
