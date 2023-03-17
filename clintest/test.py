2866032d547168b809104e5c73aef0b8bfb1eaf4
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-03-17 10:32:55 +0100

    Modify test.Inspect so that in can other tests call history

--- clintest/test.py
+++ clintest/test.py
@@ -31,66 +31,6 @@ class Test(ABC):
         pass
 
 
-class Inspect(Test):
-    def __init__(self):
-        self.artifacts = []
-        self.__outcome = True, True
-
-    def on_model(self, model: Model) -> bool:
-        self.artifacts.append({
-            "__f": "on_model",
-            "str(model)": str(model),
-        })
-        return True
-
-    def on_unsat(self, lower_bound: Sequence[int]) -> None:
-        self.artifacts.append({
-            "__f": "on_unsat",
-            "lower_bound": lower_bound,
-        })
-
-    def on_core(self, core: Sequence[int]) -> None:
-        self.artifacts.append({
-            "__f": "on_core",
-            "core": core,
-        })
-
-    def on_statistics(self, step: StatisticsMap, accumulated: StatisticsMap) -> None:
-        self.artifacts.append({
-            "__f": "on_statistics",
-            "step": step,
-            "accumulated": accumulated,
-        })
-
-    def on_finish(self, result: SolveResult) -> None:
-        self.artifacts.append({
-            "__f": "on_finish",
-            "result": result,
-        })
-        self.__outcome = Outcome(True, False)
-
-    def outcome(self) -> Outcome:
-        return self.__outcome
-
-
-class Assert(Test):
-    def __init__(self, quantifier: Quantifier, assertion: Assertion) -> None:
-        self.__quantifier = quantifier
-        self.__assertion = assertion
-
-    def on_model(self, model: Model) -> bool:
-        if not self.__quantifier.outcome().is_certain():
-            self.__quantifier.consume(self.__assertion.holds_for(model))
-
-        return not self.__quantifier.outcome().is_certain()
-
-    def on_finish(self, result: SolveResult) -> None:
-        self.__quantifier = Finished(self.__quantifier)
-
-    def outcome(self) -> Outcome:
-        return self.__quantifier.outcome()
-
-
 class True_(Test):
     def __init__(self, lazy_evaluation: bool = True) -> None:
         self.__outcome = Outcome(True, lazy_evaluation)
@@ -117,3 +57,66 @@ class False_(Test):
 
     def outcome(self) -> Outcome:
         return self.__outcome
+
+
+class Inspect(Test):
+    def __init__(self, test: Test = True_(lazy_evaluation = False)):
+        self.artifacts = []
+        self.test = test
+
+    def on_model(self, model: Model) -> bool:
+        self.artifacts.append({
+            "__f": "on_model",
+            "str(model)": str(model),
+        })
+        return self.test.on_model(model)
+
+    def on_unsat(self, lower_bound: Sequence[int]) -> None:
+        self.artifacts.append({
+            "__f": "on_unsat",
+            "lower_bound": lower_bound,
+        })
+        self.test.on_unsat(lower_bound)
+
+    def on_core(self, core: Sequence[int]) -> None:
+        self.artifacts.append({
+            "__f": "on_core",
+            "core": core,
+        })
+        self.test.on_core(core)
+
+    def on_statistics(self, step: StatisticsMap, accumulated: StatisticsMap) -> None:
+        self.artifacts.append({
+            "__f": "on_statistics",
+            "step": step,
+            "accumulated": accumulated,
+        })
+        self.test.on_statistics(step, accumulated)
+
+    def on_finish(self, result: SolveResult) -> None:
+        self.artifacts.append({
+            "__f": "on_finish",
+            "result": result,
+        })
+        self.test.on_finish(result)
+
+    def outcome(self) -> Outcome:
+        return self.test.outcome()
+
+
+class Assert(Test):
+    def __init__(self, quantifier: Quantifier, assertion: Assertion) -> None:
+        self.__quantifier = quantifier
+        self.__assertion = assertion
+
+    def on_model(self, model: Model) -> bool:
+        if not self.__quantifier.outcome().is_certain():
+            self.__quantifier.consume(self.__assertion.holds_for(model))
+
+        return not self.__quantifier.outcome().is_certain()
+
+    def on_finish(self, result: SolveResult) -> None:
+        self.__quantifier = Finished(self.__quantifier)
+
+    def outcome(self) -> Outcome:
+        return self.__quantifier.outcome()
