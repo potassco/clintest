a0b00cda3667934bbfffa751266ba4b020fc6471
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-07-24 19:15:12 +0200

    Overhaul the package documentation

--- clintest/__init__.py
+++ clintest/__init__.py
@@ -1,35 +1,34 @@
 """
 A test framework for `clingo` programs.
 
-`clintest` enables you to write efficient unit tests for `clingo` programs.
-In order to archive this, it will monitor the outcome of your test while running the solver to obtain solutions to your program.
-Once the outcome of the test becomes certain, it will automatically abort the solving process to not waste any time with unnecessary computations.
+`clintest` is a test framework written in Python that makes it easy to write efficient tests for `clingo` programs.
+It provides you with numerous off-the-shelf components that allow you to assemble the most commonly used tests quickly, saving you the time to write them yourself.
+However, should you require a hand-crafted test, it will work along the others just fine.
 
-This framework also enables you to write unit tests quickly.
-It provides you with the necessary building blocks to assemble the most commonly used tests from predefined parts.
-Thus, it saves you the hassle of writing boilerplate yourself that would otherwise drain your time.
+In order to avoid time wasted on unnecessary computations, `clintest` will monitor the outcome of your test while steering the solving process.
+Once the outcome of your test is certain, it will automatically tell the solver to abort the search for further solutions.
 
-`clintest` is a library written in Python that focusses on the specifics of `clingo` programs.
-If you are looking after functionalities that are more general to unit testing, you may want to combine `clintest` with a framework like `pytest`.
-
-There is currently no application called `clintest` but we may provide one in the future.
+As `clintest` is focussed on the specifics of `clingo` programs, it works best if you combine it with a general purpose frameworks like `pytest`.
 
 ## Installation
-This framework is guaranteed to work with Python 3.8 or greater. You have several options to install it:
+This framework is guaranteed to work with Python 3.8 or greater.
+You have several options to install it:
 
 ### Using conda
 TODO
 
 ### Using pip
-TODO
+TOOD
 
 ### From source
 TODO
 
-## Quickstart
-This section is meant to introduce you to the most important features of `clintest` using a simple example.
-Imagine you have written the program `a. {b}.` and want to ensure that all its models contain the atom `a`.
-In order to do so, you first need to create a test.
+## Quick start
+This section is meant to guide you through the most important features of `clintest` using simple examples.
+
+### Simple assertions
+Imagine you have written the program `a. {b}.` and want to ensure that all its models contains the atom `a`.
+In order to do so, you first need to create a `test.Test`.
 
 ```
 from clintest.test import Assert
@@ -39,30 +38,35 @@ from clintest.assertion import Contains
 test = Assert(All(), Contains("a"))
 ```
 
-The test `test.Assert` reviews the models of a program.
-It needs to be initialized with an assertion (here: `assertion.Contains`) that may or may not hold for a certain model and a quantifier (here: `quantifier.All`) that specifies how often the assertion must hold to pass the test.
+The test `test.Assert` inspects the models of a program.
+It needs to be initialized with an `assertion.Assertion` and a `quantifier.Quantifier`.
+An assertion (here: `assertion.Contains`) is a statement that may or may not hold for a certain model.
+A quantifier (here: `quantifier.All`) specifies how many assertions must hold in order to pass the test.
 
-Tests retain their own outcome which can be queried as follows.
+Tests retain their own outcome which may be either
+
+  - `F?` (possibly false),
+  - `T?` (possibly true),
+  - `F!` (certainly false), or
+  - `T!` (certainly true).
+
+Having an uncertain outcome means that a test has not (yet) been completed.
+Once the outcome is certain, it must no longer change in order for `clintest` to function properly.
+The outcome of a test can be queried as follows.
 
 >>> print(test.outcome())
 T?
 
-The outcome of a test may either be
-
-  - possibly false (`F?`),
-  - possibly true (`T?`),
-  - certainly false (`F!`), or
-  - certainly true (`T!`).
-
-Here, the outcome is possibly true (`T?`) as the test did not yet run but would succeed if no (further) model would be given.
-In order to run the test, we need to create a solver.
+Due to the fact this test was never run, its outcome is still uncertain.
+In order to run the test, we need to create a `solver.Solver` first.
 
 ```
 from clintest.solver import Clingo
 solver = Clingo("0", "a. {b}.")
 ```
 
-The solver `solver.Clingo` is a facade around `clingo.control.Control` which is initialized with list of arguments (here: `"0"`, meaning that the solver should compute all models) and the program (here: `"a. {b}."`).
+The solver `solver.Clingo` is a facade around `clingo.control.Control`.
+The constructor expects the program (here: `"a. {b}."`) and a list of arguments for the solver (here: `"0"`, meaning that the solver should compute all models).
 Once a solver is set up, it may solve your test as follows.
 
 >>> solver.solve(test)
@@ -70,13 +74,13 @@ Once a solver is set up, it may solve your test as follows.
 T!
 
 As you realize from the output, any model of `a. {b}.` does indeed contain the atom `a`.
-In case you want to check this within a framework like `pytest`, use the functions `outcome.Outcome` provides and the keyword `assert`.
+If you want to ensure this within a framework like `pytest`, use `clintest.test.Test.assert_` to raise an `AssertionError` with proper message if the test's outcome is not certainly true (`T!`).
 
 ```
-assert test.outcome().is_certainly_true()
+test.assert_()
 ```
 
-## Assembling compound tests
+### Compound tests
 Testing real-world programs would still require a lot of boilerplate, if `clintest` had no support for combining simple tests into more complex ones.
 The following example illustrates how to build a test that simultaneously ensures
 
@@ -99,25 +103,36 @@ test = And(
 Solving this test with our previous solver leads to the following result.
 
 >>> solver.solve(test)
->>> print(test)
-[F!] And
-    operands:
-         0: [T!] Assert
-            quantifier: Any
-            assertion:  Contains("a")
-         1: [F!] Assert
-            quantifier: All
-            assertion:  Contains("b")
-         2: [F?] Assert
-            quantifier: Any
-            assertion:  Contains("c")
-    short_circuit:  True
-    ignore_certain: True
+>>> test.assert_()
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "clintest/clintest/test.py", line 42, in assert_
+    raise AssertionError(msg)
+AssertionError: The following test has failed.
+    [F!] And
+        operands:
+             0: [T!] Assert
+                quantifier: Any
+                assertion:  Contains("a")
+             1: [F!] Assert
+                quantifier: All
+                assertion:  Contains("b")
+             2: [F?] Assert
+                quantifier: Any
+                assertion:  Contains("c")
+        short_circuit:  True
+        ignore_certain: True
 
-Printing the whole test (instead of merely the outcome) provides you with a more detailed report revealing some details about the inner workings of your tests.
-For a start, you may discover that test #2 did not run to completion as its outcome is still possibly false (`F?`).
-This is an intensional optimization as the outcome of test #2 was irrelevant for the whole test once test #1 was certainly false (`F!`).
+Because the test has failed, `clintest.test.Test.assert_` produces a rather detailed exception which may be used to explain the cause of the failure.
+Test #1 did fail, probably because there was a model that did not contain the atom `b`.
 
-## Inner workings of clintest
+But if you watch carefully, there is something else to discover.
+Test #2 was never completed.
+This is due to a deliberate optimization as the outcome of test #2 was irrelevant once the outcome of test #1 was certainly false (`F!`).
+
+### Debugging
+TODO
+
+### Hand-crafted tests
 TODO
 """
