6e12c84cb36859c99401fe89b56ba64e9a682712
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-09-04 17:14:58 +0200

    Write documentation for clintest.quantifier

--- clintest/quantifier.py
+++ clintest/quantifier.py
@@ -1,8 +1,5 @@
 """
 The abstract class `clintest.quantifier.Quantifier` and classes extending it.
-
-A quantifier specifies how many assertions must hold in order to pass the test.
-As such, one is necessary to assemble the `clintest.test.Assert` test.
 """
 
 from abc import ABC, abstractmethod
@@ -11,16 +8,47 @@ from .outcome import Outcome
 
 
 class Quantifier(ABC):
+    """
+    A quantifier specifies how many assertions must hold in order to pass the test.
+    As such, one is necessary to assemble the `clintest.test.Assert` test.
+
+    Quantifiers are stateful.
+    They consume the return values of `clintest.assertion.Assertion.holds_for` and store all the
+    information necessary to determine the current outcome of the test.
+    """
+
     @abstractmethod
     def outcome(self) -> Outcome:
-        pass
+        """
+        Returns the current outcome of this quantifier.
+
+        Returns
+        -------
+        The current outcome of this quantifier.
+        """
 
     @abstractmethod
     def consume(self, value: bool) -> Outcome:
-        pass
+        """
+        Consume the return value of `clintest.assertion.Assertion.holds_for` and possibly alter the
+        current outcome of this quantifier.
+
+        Parameters
+        ----------
+        value
+            The return value of `clintest.assertion.Assertion.holds_for`
+
+        Returns
+        -------
+        The outcome of this quantifier after `value` was consumed
+        """
 
 
 class All(Quantifier):
+    """
+    A quantifier demanding that an assertion holds for all models.
+    """
+
     def __init__(self) -> None:
         self.__state = Outcome(True, False)
 
@@ -42,6 +70,10 @@ class All(Quantifier):
 
 
 class Any(Quantifier):
+    """
+    A quantifier demanding that an assertion holds for any model.
+    """
+
     def __init__(self) -> None:
         self.__state = Outcome(False, False)
 
@@ -63,6 +95,15 @@ class Any(Quantifier):
 
 
 class Exact(Quantifier):
+    """
+    A quantifier demanding that an assertion holds for an exact number of models.
+
+    Parameters
+    ----------
+    target
+        The number of models the assertion should hold for
+    """
+
     def __init__(self, target: int) -> None:
         self.__target = target
         self.__state = 0
@@ -83,6 +124,17 @@ class Exact(Quantifier):
 
 
 class Finished(Quantifier):
+    """
+    A wrapper around an `inner` quantifier indicating that computation has finished.
+    The outcome of this quantifier is the outcome of `inner` beside that it is always certain.
+    Calling `Finished.consume` will not alter outcome of this or the `inner` quantifier.
+
+    Paramters
+    ---------
+    inner
+        The quantifier that should be finished
+    """
+
     def __init__(self, inner: Quantifier) -> None:
         self.__state = Outcome(inner.outcome().current_value(), True)
 
