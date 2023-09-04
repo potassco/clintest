771017bb2b0420b41778ec866135eaa40d2e4c4f
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2023-09-04 21:32:45 +0200

    Write documentation for clintest.test

--- clintest/test.py
+++ clintest/test.py
@@ -16,27 +16,91 @@ from .assertion import Assertion
 
 
 class Test(ABC):
+    """
+    An abstract test consuming the artifacts of a `clintest.solver.Solver` in order to compute an
+    `clintest.outcome.Outcome`.
+    """
+
     def on_model(self, _model: Model) -> bool:
+        """
+        Consume a `clingo.model.Model` and possibly alter the current outcome of this test.
+
+        Parameters
+        ----------
+        model
+            The `clingo.model.Model` to consume
+
+        Returns
+        -------
+        Whether further models a needed to decide this test.
+        """
+
         return True
 
     def on_unsat(self, lower_bound: Sequence[int]) -> None:
-        pass
+        """
+        Consume a `lower_bound` during optimization and possibly alter the current outcome of this
+        test.
+
+        Parameters
+        ----------
+        lower_bound
+            The lower bound
+        """
 
     def on_core(self, core: Sequence[int]) -> None:
-        pass
+        """
+        Consume an unsat `core` and possibly alter the current outcome of this test.
+
+        Parameters
+        ----------
+        core
+            The unsat core
+        """
 
     def on_statistics(self, step: StatisticsMap, accumulated: StatisticsMap) -> None:
-        pass
+        """
+        Consume the solving statistics and possibly alter the current outcome of this test.
+
+        Parameters
+        ----------
+        step
+            The step statistics
+
+        accumulated
+            The accumulated statistics
+        """
 
     @abstractmethod
     def on_finish(self, result: SolveResult) -> None:
-        pass
+        """
+        Consume the final solve result and possibly alter the current outcome of this test.
+
+        This should be the last `on_*`-method ever called on a test.
+        Afterwards the outcome must be certain.
+
+        Parameters
+        ----------
+        result
+            The `clingo.solving.SolveResult`
+        """
 
     @abstractmethod
     def outcome(self) -> Outcome:
-        pass
+        """
+        Returns the current `Outcome` of this test.
+
+        Returns
+        -------
+        The current outcome of this test.
+        """
 
     def assert_(self) -> None:
+        """
+        Assert the outcome of this test to be certainly true.
+        Raise an `AssertionError` if the test is either incomplete or has failed.
+        """
+
         if not self.outcome().is_certainly_true():
             msg = "The following test "
             msg += ["is incomplete.", "has failed."][self.outcome().is_certain()]
@@ -47,6 +111,15 @@ class Test(ABC):
 
 
 class True_(Test):
+    """
+    The test which always succeeds.
+
+    Parameters
+    ----------
+    lazy
+        Whether this test should be lazy, i.e., not consume any models.
+    """
+
     def __init__(self, lazy: bool = True) -> None:
         self.__outcome = Outcome(True, lazy)
 
@@ -69,6 +142,15 @@ class True_(Test):
 
 
 class False_(Test):
+    """
+    The test which always failes.
+
+    Parameters
+    ----------
+    lazy
+        Whether this test should be lazy, i.e., not consume any models.
+    """
+
     def __init__(self, lazy: bool = True) -> None:
         self.__outcome = Outcome(False, lazy)
 
@@ -91,6 +173,11 @@ class False_(Test):
 
 
 class Recording:
+    """
+    A recording of the calls to the `on_*`-methods of a `Test`.
+    This class is mainly used inside of `Record`.
+    """
+
     def __init__(self, entries: Optional[Sequence[Dict[str, Any]]] = None):
         if entries is None:
             entries = []
@@ -118,12 +205,39 @@ class Recording:
         return self.__entries == other.__entries
 
     def amend(self, changes: Dict[str, Any]):
+        """
+        Update the last entry of this encoding.
+
+        Parameters
+        ----------
+        changes
+            The changes
+        """
+
         self.__entries[-1].update(changes)
 
     def append(self, entry: Dict[str, Any]):
+        """
+        Append a new entry at the end of this recording.
+
+        Parameters
+        ----------
+        entry
+            The entry
+        """
+
         self.__entries.append(entry)
 
     def subsumes(self, other) -> bool:
+        """
+        Determine whether this recording subsumes another recording.
+
+        Parameters
+        ----------
+        other
+            The other recording
+        """
+
         # pylint: disable=protected-access
         return len(self.__entries) == len(other.__entries) and all(
             all(item in other_entry.items() for item in self_entry.items())
@@ -132,6 +246,16 @@ class Recording:
 
 
 class Record(Test):
+    """
+    A test that behaves identical to a given other `test` but records any call to one of it
+    `on_*`-methods. This can be very helpful for debugging.
+
+    Parameters
+    ----------
+    test
+        A `Test` that determines how this test should behave.
+    """
+
     def __init__(self, test: Test = True_(lazy = False)):
         self.test: Test = test
         self.recording: Recording = Recording([{
@@ -203,6 +327,20 @@ class Record(Test):
 
 
 class Assert(Test):
+    """
+    A test that asserts certain properties about the `clingo.model.Model`s of a program. This test
+    can be highly costumized using a `clintest.quantifier.Quantifier` and a
+    `clintest.assertion.Assertion`.
+
+    Parameters
+    ----------
+    quantifier
+        The `clintest.quantifier.Quantifier` used with this test.
+
+    assertion
+        The `clintest.assertion.Assertion` used with this test.
+    """
+
     def __init__(self, quantifier: Quantifier, assertion: Assertion) -> None:
         self.__quantifier = quantifier
         self.__assertion = assertion
@@ -234,6 +372,16 @@ class Assert(Test):
 
 
 class Not(Test):
+    """
+    The negation of a given test.
+    This test failes if `operand` succeeds and vice versa.
+
+    Parameters
+    ----------
+    operand
+        The `Test` to be negated
+    """
+
     def __init__(self, operand: Test) -> None:
         self.__operand = operand
 
@@ -269,6 +417,24 @@ class Not(Test):
 
 
 class And(Test):
+    """
+    The conjunction of a list given tests.
+    This test succeeds if all `args` succeed.
+
+    Parameters
+    ----------
+    args
+        The `Test`s to be combined.
+
+    short_circuit
+        Whether this test should employ short circuit optimization, i.e., abort all remaining tests
+        once the outcome of a test is certainly false.
+
+    ignore_certain
+        Whether this test should employ the ignore certain optimization, i.e., not send artifacts
+        to test that are already certain.
+    """
+
     def __init__(
         self,
         *args: Test,
@@ -390,6 +556,24 @@ class And(Test):
 
 
 class Or(Test):
+    """
+    The disjunction of a list given tests.
+    This test succeeds if any `args` succeed.
+
+    Parameters
+    ----------
+    args
+        The `Test`s to be combined.
+
+    short_circuit
+        Whether this test should employ short circuit optimization, i.e., abort all remaining tests
+        once the outcome of a test is certainly true.
+
+    ignore_certain
+        Whether this test should employ the ignore certain optimization, i.e., not send artifacts
+        to test that are already certain.
+    """
+
     def __init__(
         self,
         *args: Test,
