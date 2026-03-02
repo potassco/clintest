b8030c89b8d15c1546cf4a0698f9a2e7c65bc79c
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2026-03-02 12:24:25 +0100

    Configure ruff

--- tests/test_quantifier.py
+++ tests/test_quantifier.py
@@ -1,7 +1,3 @@
-# pylint: disable=import-outside-toplevel
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
 # pylint: disable=redefined-builtin
 
 
